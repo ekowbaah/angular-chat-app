f0459a94313ca588c656aa3cbadb4fe1f226d238
Ekow Baah-Nyarkoh <ekow.baahnyarkoh@gi-de.com>
2020-03-03 15:17:15 +0100

    downgrade to angular 4

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCbCBScxl5m9brL7DX2hY8gmIqagR3fC2M',
+    authDomain: 'newchatapp-a4ff5.firebaseapp.com',
+    databaseURL: 'https://newchatapp-a4ff5.firebaseio.com',
+    projectId: 'newchatapp-a4ff5',
+    storageBucket: 'newchatapp-a4ff5.appspot.com',
+    messagingSenderId: '941164755093'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCbCBScxl5m9brL7DX2hY8gmIqagR3fC2M',
+    authDomain: 'newchatapp-a4ff5.firebaseapp.com',
+    databaseURL: 'https://newchatapp-a4ff5.firebaseio.com',
+    projectId: 'newchatapp-a4ff5',
+    storageBucket: 'newchatapp-a4ff5.appspot.com',
+    messagingSenderId: '941164755093'
+  }
+};
